737401003a43f239aedb42628d38e76b0c76f196
alejandrobrown <ballve@gmail.com>
2020-08-19 11:52:53 +0000

    Update bio-content.js

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -2,15 +2,13 @@ import React, { Fragment } from "react"
 import { Styled } from "theme-ui"
 
 /**
- * Change the content to add your own bio
+ * Squatting in the American wilds
  */
 
 export default () => (
   <Fragment>
-    This is where <Styled.a href="http://example.com/">your name</Styled.a>
+   <Styled.a href="http://example.com/">Alejandro Brown</Styled.a>
     {` `}
-    goes.
     <br />
-    Or whatever, you make the rules.
   </Fragment>
 )
